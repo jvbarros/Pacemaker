34977c0e2a400f0b3f6defc51d42a338dd4a1955
Jacson Barros <jacson.barros@gmail.com>
2012-11-27 07:32:54 -0200

    Insert default file parameters

--- pacemaker.R
+++ pacemaker.R
@@ -17,16 +17,22 @@ library("RMySQL")
 
 ######### if you are using MYSQL DATA BASE
 
-# 1. Create a Connection
+# Here, you can specify user authentication parameters through a MySQL default.file
 
-con <- dbConnect(MySQL(), user="jacson_barros", password="p8z77-v", dbname="susdb", host="ec2-50-19-4-203.compute-1.amazonaws.com")
+strPath <- "/Users/Administrator/Documents/Projects/R/Pacemaker/"
+strConf <- "susdb.cnf"
+
+# Create a Connection
+
+con <- dbConnect(MySQL(), group = "susdb" , default.file= paste( strPath , strConf , sep="") )
+
 
 sql <- "select  N_AIH, ANO_CMPT , MES_CMPT , DT_INTER, DT_SAIDA, DIAS_PERM, MUNIC_RES , 
                 tab.UF, tab.nm_municipio, CEP , NASC , IDADE, SEXO , PROC_REA , DIAG_PRINC, DIAG_SECUN , 
                 VAL_SH, VAL_SP , VAL_TOT , US_TOT 
           from T2010 aih , censodb.tab_municipio tab 
          where proc_rea in ( '0406010650', '0406010684')  
-           and aih.munic_res = substr(tab.cd_municipio,1,6) LIMIT 10000;"
+           and aih.munic_res = substr(tab.cd_municipio,1,6);"
 
 df <- dbGetQuery( con , sql)
 
@@ -39,6 +45,8 @@ dbDisconnect(con)
 ## Summary by group UF
 
 tapply(df$VAL_TOT, df$UF, summary)
+
+
 
 
 
